b35e5413951ac522682d11598396c9320d67cf68
Bao Nguyen <baonguyen@navapbc.com>
2025-10-20 08:27:02 -0600

    Migrate indices on application forms

---
+++ reporting-app/db/migrate/20251020141545_add_unique_indices_on_application_forms.rb
@@ -0,0 +1,13 @@
+class AddUniqueIndicesOnApplicationForms < ActiveRecord::Migration[7.2]
+  def change
+    remove_index :activity_report_application_forms, :certification_case_id
+    add_index :activity_report_application_forms, :certification_case_id, unique: true
+
+    remove_index :exemption_application_forms, :certification_case_id
+    add_index :exemption_application_forms, :certification_case_id, unique: true
+
+
+    remove_column :activity_report_application_forms, :certification_id
+    remove_column :exemption_application_forms, :certification_id
+  end
+end
