1cb65324c144b4f2124d117bee225d0b0031d18e
Loren Yu <loren@navapbc.com>
2025-10-20 14:28:54 -0700

    Don't show unreported income

--- reporting-app/app/models/activity.rb
+++ reporting-app/app/models/activity.rb
@@ -10,11 +10,8 @@ class Activity < ApplicationRecord
   strata_attribute :month, :date
   strata_attribute :hours, :decimal
   strata_attribute :name, :string
+  strata_attribute :income_earned, :decimal
 
   validates :name, presence: true
   validates :hours, presence: true, numericality: { greater_than: 0 }
-
-  def income_earned
-    hours * 7.25
-  end
 end
