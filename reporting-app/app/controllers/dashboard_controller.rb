31d5a122cb8526c83531bf3d12ec7a7e12eb8993
baonguyenNava <baonguyen@navapbc.com>
2025-10-20 11:28:00 -0600

    Add Member views- Request for Information (#101)
    ## Ticket
    
    Resolves
    [TSS-366](https://linear.app/nava-platform/issue/TSS-366/member-experience-request-for-information)
    
    ## Changes
    
    Add views to support Member RFI
    
    ## Context for reviewers
    
    After an application is submitted, a caseworker can request more
    information. This completes the member side RFI actions. However, it
    does not notify the caseworker that the member has completed the RFI.
    
    ## Testing
    ![2025-10-20 at 6 46 55 - Exemption
    RFI](https://github.com/user-attachments/assets/a93d3b6d-49ce-4f8a-9055-33db096daa9a)
    ![2025-10-20 at 6 32 54 - Activity Report
    RFI](https://github.com/user-attachments/assets/3c74981d-1ed9-47ec-8f3e-e788d98869c3)
    
    <img width="999" height="644" alt="Screenshot 2025-10-20 at 6 48 36 AM"
    src="https://github.com/user-attachments/assets/aa5a6e94-9844-49ab-a92c-56de22abe1fa"
    />
    <img width="997" height="929" alt="Screenshot 2025-10-20 at 6 48 47 AM"
    src="https://github.com/user-attachments/assets/803b9d0c-2388-4b8c-8245-133ef1d5efad"
    />
    <img width="1017" height="458" alt="Screenshot 2025-10-20 at 6 49 03 AM"
    src="https://github.com/user-attachments/assets/b4c04267-dc49-4f5f-b038-792743151946"
    />
    
    
    
    <!-- reporting-app - begin PR environment info -->
    ## Preview environment for reporting-app
    - Service endpoint:
    https://p-101-reporting-app-dev-958713094.us-east-1.elb.amazonaws.com
    - Deployed commit: 68c3bdf1d0d5ba05d317091315ac8f198583e00b
    <!-- reporting-app - end PR environment info -->
    
    ---------
    
    Co-authored-by: Mike Giver <mikegiver@navapbc.com>

--- reporting-app/app/controllers/dashboard_controller.rb
+++ reporting-app/app/controllers/dashboard_controller.rb
@@ -2,18 +2,42 @@
 
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_certifications
+  before_action :set_certification_case, if: -> { @certification.present? }
+  before_action :set_exemption_application_form, if: -> { @certification_case.present? }
+  before_action :set_activity_report_application_form, if: -> { @certification_case.present? }
+  before_action :set_information_requests, if: -> { @exemption_application_form.present? || @activity_report_application_form.present? }
+
 
   # TODO: figure out authz
   skip_after_action :verify_policy_scoped
 
-  # GET /dashboard or /dashboard.json
   def index
+  end
+
+  private
+
+  def set_certifications
     @all_certifications = Certification.find_by_member_email(current_user.email).order(created_at: :desc).all
     @certification = @all_certifications.first
+  end
+
+  def set_certification_case
     @certification_case = CertificationCase.find_by(certification_id: @certification&.id)
-    if @certification_case
-      @exemption_application_form = ExemptionApplicationForm.where(certification_case_id: @certification_case&.id).order(created_at: :desc).first
-      @activity_report_application_form = policy_scope(ActivityReportApplicationForm).where(certification_case_id: @certification_case&.id).order(created_at: :desc).first
-    end
+  end
+
+  def set_exemption_application_form
+    @exemption_application_form = ExemptionApplicationForm.find_by_certification_case_id(@certification_case&.id)
+  end
+
+  def set_activity_report_application_form
+    @activity_report_application_form = ActivityReportApplicationForm.find_by_certification_case_id(@certification_case&.id)
+  end
+
+  def set_information_requests
+    # TODO: The UI does not allow for multiple forms to be submitted, but the data model allows for it.
+    # There should only be one information request possible for all application forms.
+    @information_requests = InformationRequest.for_application_forms([ @exemption_application_form&.id, @activity_report_application_form&.id ].compact)
+    @information_request = @information_requests.select { |request| request.member_comment.blank? && request.supporting_documents.empty? }.first
   end
 end
