29602d1b9476655d52e6e2a0afd929e02a9e7205
baonguyenNava <baonguyen@navapbc.com>
2025-10-20 09:19:38 -0600

    Update reporting-app/app/controllers/exemption_information_requests_controller.rb
    Co-authored-by: Michael Giver <mikegiver@navapbc.com>

--- reporting-app/app/controllers/exemption_information_requests_controller.rb
+++ reporting-app/app/controllers/exemption_information_requests_controller.rb
@@ -2,7 +2,7 @@
 
 class ExemptionInformationRequestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_information_request, only: %i[ show edit update ]
+  before_action :set_information_request, only: %i[ edit update ]
 
   def show
   end
