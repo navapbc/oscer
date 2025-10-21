2353f51e5899c41b4691ecb9faaa0dff394d92d6
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-10-21 12:16:02 -0500

    Update seeder

--- reporting-app/db/seeds/development.rb
+++ reporting-app/db/seeds/development.rb
@@ -15,6 +15,7 @@
   app_form.activities.create!(
     name: "Community Meeting",
     month: Date.today.prev_month.beginning_of_month,
+    type: "HourlyActivity",
     hours: rand(1..5),
     supporting_documents: [
       { io: File.open(Rails.root.join("db/seeds/files/fake_paystub.png")), filename: "Courthouse Clerk Paystub.png", content_type: "image/png" }
@@ -23,7 +24,8 @@
   app_form.activities.create!(
     name: "Outreach Event",
     month: Date.today.prev_month.beginning_of_month,
-    hours: rand(1..5),
+    type: "HourlyActivity",
+    hours: rand(1.0..5),
     supporting_documents: [
       { io: File.open(Rails.root.join("db/seeds/files/fake_paystub.pdf")), filename: "Paystub1.pdf", content_type: "application/pdf" },
       { io: File.open(Rails.root.join("db/seeds/files/fake_paystub.png")), filename: "Paystub2.png", content_type: "image/png" },
@@ -33,7 +35,8 @@
   app_form.activities.create!(
     name: "Training Session",
     month: Date.today.prev_month.beginning_of_month,
-    hours: rand(1..5),
+    type: "EarnedIncomeActivity",
+    earned_income: rand(25..250),
     supporting_documents: [
       { io: File.open(Rails.root.join("db/seeds/files/fake_training_certificate.pdf")), filename: "Training Certificate.pdf", content_type: "application/pdf" }
     ]
@@ -41,11 +44,13 @@
   app_form.activities.create!(
     name: "Policy Discussion",
     month: Date.today.prev_month.prev_month.beginning_of_month,
+    type: "HourlyActivity",
     hours: rand(1..10)
   )
   app_form.activities.create!(
     name: "Volunteer Coordination",
     month: Date.today.prev_month.prev_month.beginning_of_month,
+    type: "HourlyActivity",
     hours: rand(15..60),
     supporting_documents: [
       { io: File.open(Rails.root.join("db/seeds/files/fake_paystub.pdf")), filename: "Administrative Paystub.pdf", content_type: "application/pdf" },
