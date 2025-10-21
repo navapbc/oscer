35939074672d84bf608cd7f37733a1d4d59d8dba
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-10-21 12:29:07 -0500

    Add validation for earned income

--- reporting-app/app/models/earned_income_activity.rb
+++ reporting-app/app/models/earned_income_activity.rb
@@ -2,4 +2,5 @@ class EarnedIncomeActivity < Activity
   include Strata::Attributes
 
   strata_attribute :earned_income, :money
+  validates :earned_income, presence: true, numericality: { greater_than: 0 }
 end
