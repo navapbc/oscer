23f7c99b0986f2cf71d9774234c08bd91588baa1
Tanner Doshier <git@doshitan.com>
2025-10-27 13:11:58 -0400

    combine some params into "info" hashes for clearer code

--- reporting-app/lib/active_model/validations/attributes_type_validator.rb
+++ reporting-app/lib/active_model/validations/attributes_type_validator.rb
@@ -49,16 +49,22 @@ module ActiveModel
         attr_type = record.class.type_for_attribute(attribute_name)
         expected_value_types = get_underlying_types_for_attribute_type(attr_type)
 
-        check_value_for_types(record, record.errors, attribute_name, value, raw_value, expected_value_types)
+        check_value_for_types(
+          record,
+          record.errors,
+          { name: attribute_name, type: attr_type },
+          { final: value, raw: raw_value },
+          expected_value_types
+        )
       end
 
-      def check_value_for_types(record, errors, attribute_name, value, raw_value, expected_types)
+      def check_value_for_types(record, errors, attr_info, value_info, expected_types)
         type_errors = {}
         for expected_type in expected_types
           errs = ActiveModel::Errors.new(record)
           type_errors[expected_type.to_s] = errs
 
-          check_value_for_type(record, errs, attribute_name, value, raw_value, expected_type)
+          check_value_for_type(record, errs, attr_info, value_info, expected_type)
         end
 
         types_with_errors = type_errors.reject { |key, value| value.empty? }
@@ -69,75 +75,81 @@ module ActiveModel
         end
       end
 
-      def check_value_for_type(record, errors, attribute_name, value, raw_value, expected_type)
+      def check_value_for_type(record, errors, attr_info, value_info, expected_type)
         if expected_type <= Enumerable
-          if !value.is_a?(Enumerable) || !raw_value.is_a?(Enumerable)
-            errors.add(attribute_name, "invalid_value")
+          if !value_info[:final].is_a?(Enumerable) || !value_info[:raw].is_a?(Enumerable)
+            errors.add(attr_info[:name], "invalid_value")
             return
           end
 
-          if value.count != raw_value.count
-            errors.add(attribute_name, "invalid_item_value")
+          if value_info[:final].count != value_info[:raw].count
+            errors.add(attr_info[:name], "invalid_item_value")
             return
           end
 
           # TODO: generalize support for this?
           # if the values are supposed to be of a particular type
-          if attr_type.respond_to?(:of)
-            item_type = attr_type.of
+          if attr_info[:type].respond_to?(:of)
+            item_type = attr_info[:type].of
           end
-          if attr_type.respond_to?(:item_class)
-            item_type = attr_type.item_class
+          if attr_info[:type].respond_to?(:item_class)
+            item_type = attr_info[:type].item_class
           end
 
           if item_type.present?
-            value.each_with_index do |item, index|
-              check_value_for_types(record, errors, attribute_name + "[#{index}]", item, raw_value[index], get_underlying_types_for_attribute_type(item_type))
+            value_info[:final].each_with_index do |item, index|
+              check_value_for_types(
+                record,
+                errors,
+                attr_info.merge({ name: attr_info[:name] + "[#{index}]" }),
+                { final: item, raw: value_info[:raw][index] },
+                get_underlying_types_for_attribute_type(item_type)
+              )
             end
           end
         else
-          check_value_for_type_item(record, errors, attribute_name, value, raw_value, expected_type)
+          check_value_for_type_item(record, errors, attr_info, value_info, expected_type)
         end
       end
 
-      def check_value_for_type_item(record, errors, attribute_name, value, raw_value, expected_type)
+      def check_value_for_type_item(record, errors, attr_info, value_info, expected_type)
         # If model.<attribute> is nil, but model.<attribute>_before_type_cast is
         # not nil, that means the application failed to cast the value to the
         # appropriate type in order to complete the attribute assignment. This
         # means the original value is invalid.
-        did_type_cast_fail = value.nil? && !raw_value.nil?
+        did_type_cast_fail = value_info[:final].nil? && !value_info[:raw].nil?
         if did_type_cast_fail
-          errors.add(attribute_name, "invalid_value")
+          errors.add(attr_info[:name], "invalid_value")
           return
         end
 
-        is_expected_type = value.is_a?(expected_type)
-        if !is_expected_type && !raw_value.blank?
-          errors.add(attribute_name, "invalid_value")
+        is_expected_type = value_info[:final].is_a?(expected_type)
+        if !is_expected_type && !value_info[:raw].blank?
+          errors.add(attr_info[:name], "invalid_value")
           return
         end
 
         # don't let any old value just come through as a string
         # TODO: create better/more strict ActiveModel::Type::String class that avoids this natively
-        if expected_type == String && !raw_value.nil? && !raw_value.is_a?(String)
-          errors.add(attribute_name, "invalid_value")
+        if expected_type == String && !value_info[:raw].nil? && !value_info[:raw].is_a?(String)
+          errors.add(attr_info[:name], "invalid_value")
           return
         end
 
         # don't let non-numeric strings masquerade as a proper number
         # TODO: create better/more strict ActiveModel::Type::Integer class that avoids this natively
-        if expected_type == Integer && !raw_value.nil?
-          non_numeric_string = value.zero? && (raw_value != "0" || raw_value != 0)
-          boolean = (raw_value.is_a?(TrueClass) || raw_value.is_a?(FalseClass))
+        if expected_type == Integer && !value_info[:raw].nil?
+          non_numeric_string = value_info[:final].zero? && (value_info[:raw] != "0" || value_info[:raw] != 0)
+          boolean = (value_info[:raw].is_a?(TrueClass) || value_info[:raw].is_a?(FalseClass))
 
           if non_numeric_string || boolean
-            errors.add(attribute_name, "invalid_value")
+            errors.add(attr_info[:name], "invalid_value")
           end
         end
 
         # don't let random input map to a proper boolean
         # TODO: create better/more strict ActiveModel::Type::Boolean class that avoids this natively
-        if (expected_type == TrueClass || expected_type == FalseClass) && !raw_value.nil?
+        if (expected_type == TrueClass || expected_type == FalseClass) && !value_info[:raw].nil?
           false_values = ActiveModel::Type::Boolean::FALSE_VALUES
           true_values = [
             true, 1,
@@ -150,8 +162,8 @@ module ActiveModel
             "off", :off
           ].to_set.freeze
 
-          if !(false_values|true_values).include?(raw_value)
-            errors.add(attribute_name, "invalid_value")
+          if !(false_values|true_values).include?(value_info[:raw])
+            errors.add(attr_info[:name], "invalid_value")
           end
         end
       end
