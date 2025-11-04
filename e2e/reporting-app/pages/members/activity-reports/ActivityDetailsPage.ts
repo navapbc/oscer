90304faf549d1df4ca333583750f8bd0b12bfad9
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-11-04 14:50:35 -0600

    Cleanup formatting

--- e2e/reporting-app/pages/members/activity-reports/ActivityDetailsPage.ts
+++ e2e/reporting-app/pages/members/activity-reports/ActivityDetailsPage.ts
@@ -1,7 +1,4 @@
-import {
-  Locator,
-  Page,
-} from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
 import { BasePage } from '../../BasePage';
 import { SupportingDocumentsPage } from './SupportingDocumentsPage';
