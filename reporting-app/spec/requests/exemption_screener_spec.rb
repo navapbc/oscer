74c1293ba806c24493a59016b43d36a48b71cc75
Tanner Doshier <git@doshitan.com>
2025-11-05 22:42:56 +0000

    Add exemption screener page (#12)

---
+++ reporting-app/spec/requests/exemption_screener_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe "ExemptionScreeners", type: :request do
+  include Warden::Test::Helpers
+
+  let(:user) { User.create!(email: "test@example.com", uid: SecureRandom.uuid, provider: "login.gov") }
+  let(:certification) { create(:certification, :connected_to_email, email: user.email) }
+  let(:certification_case) { create(:certification_case, certification: certification) }
+
+  before do
+    login_as user
+  end
+
+  describe "GET /index" do
+    it "returns http success when given certification case" do
+      get exemption_screener_path(certification_case_id: certification_case.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns http redirect if not given certification case" do
+      get exemption_screener_path
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+end
