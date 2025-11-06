e9bb564e6100596f7495bb1168ca58c8b0e57336
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-11-06 13:34:48 -0600

    Regenerate schema

--- reporting-app/db/schema.rb
+++ reporting-app/db/schema.rb
@@ -67,7 +67,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_31_214909) do
 
   create_table "certification_batch_uploads", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "filename", null: false
-    t.integer "status", default: 0, null: false
+    t.string "status", default: "pending", null: false
     t.uuid "uploader_id", null: false
     t.integer "num_rows", default: 0
     t.integer "num_rows_processed", default: 0
@@ -78,7 +78,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_31_214909) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["created_at"], name: "index_certification_batch_uploads_on_created_at"
-    t.index ["status"], name: "index_certification_batch_uploads_on_status"
     t.index ["uploader_id"], name: "index_certification_batch_uploads_on_uploader_id"
   end
 
