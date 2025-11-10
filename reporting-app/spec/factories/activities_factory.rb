ef19af03e1e8713c39db17e827cd0a170c0a1b06
baonguyenNava <baonguyen@navapbc.com>
2025-11-10 11:27:21 -0700

    Update reporting-app/spec/factories/activities_factory.rb
    Co-authored-by: Michael Giver <mikegiver@navapbc.com>

--- reporting-app/spec/factories/activities_factory.rb
+++ reporting-app/spec/factories/activities_factory.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :activity do
     month { Date.today.prev_month.beginning_of_month }
     name { Faker::Company.name }
-    category { %w[employment education community_service].sample }
+    category { Activity::ALLOWED_CATAGORIES.sample }
 
     factory :hourly_activity, parent: :activity, class: HourlyActivity do
       type { "HourlyActivity" }
