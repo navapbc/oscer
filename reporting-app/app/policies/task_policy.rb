2e74e0074074b77106e93c6fe80ce50a5a88d570
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-12-11 14:52:59 -0600

    Created policy for tasks

---
+++ reporting-app/app/policies/task_policy.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+class TaskPolicy < ApplicationPolicy
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    staff?
+  end
+
+  def show?
+    staff_in_region?
+  end
+
+  def update?
+    staff_in_region?
+  end
+
+  def pick_up_next_task?
+    staff?
+  end
+
+  def assign?
+    staff_in_region?
+  end
+
+  def request_information?
+    staff_in_region?
+  end
+
+  def create_information_request?
+    staff_in_region?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope
+      .joins("INNER JOIN certification_cases ON certification_cases.id = strata_tasks.case_id")
+      .joins("INNER JOIN certifications ON certifications.id = certification_cases.certification_id")
+      .where("certifications.certification_requirements->>'region' = ?", user.region) 
+    end
+  end
+
+  private
+
+  def staff?
+    user.staff?
+  end
+
+  def in_region?
+    user.region == TaskService.get_region_for_task(record)
+  end
+
+  def staff_in_region?
+    staff? && in_region?
+  end
+end
