b830ca8b862b4912775756b499c808df08dda958
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-12-11 14:57:58 -0600

    Removed unnecessary code

--- reporting-app/app/policies/task_policy.rb
+++ reporting-app/app/policies/task_policy.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class TaskPolicy < ApplicationPolicy
-  def initialize(user, record)
-    @user = user
-    @record = record
-  end
-
   def index?
     staff?
   end
