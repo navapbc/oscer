ed7b6cf4d15f67f7f6bdabcc3eaeba492d103554
Nader Kutub <nkutub@users.noreply.github.com>
2025-12-11 22:31:08 -0700

    Update reporting-app/app/models/determination.rb
    Co-authored-by: Michael Giver <mikegiver@navapbc.com>

--- reporting-app/app/models/determination.rb
+++ reporting-app/app/models/determination.rb
@@ -38,7 +38,7 @@ class Determination < Strata::Determination
     is_american_indian_or_alaska_native: "american_indian_alaska_native_exempt",
     income_reported_compliant: "income_reported_compliant",
     hours_reported_compliant: "hours_reported_compliant",
-    hours_insufficient: "hours_insufficient",
+    hours_reported_insufficient: "hours_reported_insufficient",
     exemption_request_compliant: "exemption_request_compliant"
   }.freeze
 
