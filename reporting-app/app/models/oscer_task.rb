788ce7f35d3318648b0b0f9775a8e59b72055f57
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-12-12 12:03:09 -0600

    Remove commented code

--- reporting-app/app/models/oscer_task.rb
+++ reporting-app/app/models/oscer_task.rb
@@ -4,13 +4,6 @@ class OscerTask < Strata::Task
   # TODO: Figure out a better way to handle default due dates for tasks
   attribute :due_on, :date, default: -> { 7.days.from_now.to_date }
 
-  # This doesn't currently work because it adds a WHERE for task.type = 'OscerTask', which finds no records
-  # scope :for_region, ->(region) {
-  #   joins("INNER JOIN certification_cases ON certification_cases.id = strata_tasks.case_id")
-  #   .joins("INNER JOIN certifications ON certifications.id = certification_cases.certification_id")
-  #   .where("certifications.certification_requirements->>'region' = ?", region)
-  # }
-
   def self.policy_class
     TaskPolicy
   end
