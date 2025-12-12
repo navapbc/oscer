64f09b021cc135f37efc258e16476bf2ea0d9d05
Nader Kutub <naderkutub@navapbc.com>
2025-12-11 22:42:37 -0700

    change to hours_reported_insufficient

--- reporting-app/spec/models/determination_spec.rb
+++ reporting-app/spec/models/determination_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe Determination, type: :model do
           american_indian_alaska_native_exempt
           income_reported_compliant
           hours_reported_compliant
-          hours_insufficient
+          hours_reported_insufficient
           exemption_request_compliant
         ]
         expect(Determination::VALID_REASONS).to match_array(expected_reasons)
@@ -67,16 +67,14 @@ RSpec.describe Determination, type: :model do
         expect(determination.errors[:reasons]).to include(match(/must contain only valid reason values/))
       end
 
-      context 'hours-based reason codes' do
-        it 'accepts hours_reported_compliant' do
-          determination = build(:determination, reasons: ['hours_reported_compliant'])
-          expect(determination).to be_valid
-        end
+      it 'accepts hours_reported_compliant' do
+        determination = build(:determination, reasons: [ 'hours_reported_compliant' ])
+        expect(determination).to be_valid
+      end
 
-        it 'accepts hours_insufficient' do
-          determination = build(:determination, reasons: ['hours_insufficient'])
-          expect(determination).to be_valid
-        end
+      it 'accepts hours_insufficient' do
+        determination = build(:determination, reasons: [ 'hours_reported_insufficient' ])
+        expect(determination).to be_valid
       end
     end
   end
