1e7dfc5396f0e7363c1f1c95797eeea009555e8e
Mike Giver <mikegiver@navapbc.com>
2026-01-08 16:57:20 -0500

    disable exec access

--- infra/reporting-app/app-config/dev.tf
+++ infra/reporting-app/app-config/dev.tf
@@ -21,7 +21,7 @@ module "dev_config" {
   # Enables ECS Exec access for debugging or jump access.
   # See https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-exec.html
   # Defaults to `false`. Uncomment the next line to enable.
-  enable_command_execution = true
+  # enable_command_execution = true
 
   # Uncomment to override default feature flag values
   # feature_flag_overrides = {
