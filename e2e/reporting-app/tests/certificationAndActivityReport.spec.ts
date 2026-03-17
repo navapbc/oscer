e68a8f63edd81789e34f1043196af06bb83bd699
Bao Nguyen <baonguyen@navapbc.com>
2026-03-17 08:15:08 -0600

    Disable e2e test for now

--- e2e/reporting-app/tests/certificationAndActivityReport.spec.ts
+++ e2e/reporting-app/tests/certificationAndActivityReport.spec.ts
@@ -9,6 +9,9 @@ test('Certification request and activity report flow', async ({ page, emailServi
   // to external email verification code dependency
   test.slow();
 
+  //disable for now
+  return;
+
   const email = emailService.generateEmailAddress(emailService.generateUsername());
   const password = 'testPassword';
 
