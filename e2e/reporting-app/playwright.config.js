1f965ef413611c3b469a29c95692974faa4a1975
Bao Nguyen <baonguyen@navapbc.com>
2026-03-19 09:49:05 -0600

    Update testing config

--- e2e/reporting-app/playwright.config.js
+++ e2e/reporting-app/playwright.config.js
@@ -1,11 +1,19 @@
 import baseConfig from '../playwright.config';
 import { deepMerge } from '../lib/util';
 import { defineConfig } from '@playwright/test';
+import path from 'path';
 
 export default defineConfig(
   deepMerge(baseConfig, {
+    timeout: 60000,
+    testDir: path.join(__dirname, 'tests'),
     use: {
-      baseURL: baseConfig.use.baseURL || 'localhost:3000',
+      baseURL: baseConfig.use.baseURL || 'http://localhost:3000',
+    },
+    webServer: {
+      command: 'node e2e/reporting-app/support/mockDocAiServer.js',
+      port: 3001,
+      reuseExistingServer: true,
     },
   })
 );
