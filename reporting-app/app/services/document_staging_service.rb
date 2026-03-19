9e426be073f5941fc9136d601e1dbded18752e23
Bao Nguyen <baonguyen@navapbc.com>
2026-03-19 09:48:17 -0600

    Add configurable job delay to document staging service

--- reporting-app/app/services/document_staging_service.rb
+++ reporting-app/app/services/document_staging_service.rb
@@ -37,7 +37,8 @@ class DocumentStagingService
 
     pending_ids = staged_documents.select(&:pending?).map(&:id)
     if pending_ids.any?
-      FetchDocAiResultsJob.set(wait: 1.minute).perform_later(pending_ids)
+      delay = ENV.fetch("DOC_AI_JOB_DELAY_SECONDS", "60").to_i.seconds
+      FetchDocAiResultsJob.set(wait: delay).perform_later(pending_ids)
     end
 
     staged_documents
