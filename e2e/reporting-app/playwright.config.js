6674aa99bf5c2e4f5dee200826b0f8515b5ed20e
Bao Nguyen <baonguyen@navapbc.com>
2026-03-20 12:50:36 -0600

    Cleanup unnecessary change

--- e2e/reporting-app/playwright.config.js
+++ e2e/reporting-app/playwright.config.js
@@ -1,14 +1,11 @@
 import baseConfig from '../playwright.config';
 import { deepMerge } from '../lib/util';
 import { defineConfig } from '@playwright/test';
-import path from 'path';
 
 export default defineConfig(
   deepMerge(baseConfig, {
-    timeout: 60000,
-    testDir: path.join(__dirname, 'tests'),
     use: {
-      baseURL: baseConfig.use.baseURL || 'http://localhost:3000',
+      baseURL: baseConfig.use.baseURL || 'ocalhost:3000',
     },
   })
 );
