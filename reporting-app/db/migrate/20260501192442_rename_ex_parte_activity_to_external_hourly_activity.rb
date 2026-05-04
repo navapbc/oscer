8d7ac94399454d44704bada37b1d9e580e65e36d
Tomas Apodaca <45415133+tomas-nava@users.noreply.github.com>
2026-05-04 14:23:08 -0700

    remove `to:`s from migrations

--- reporting-app/db/migrate/20260501192442_rename_ex_parte_activity_to_external_hourly_activity.rb
+++ reporting-app/db/migrate/20260501192442_rename_ex_parte_activity_to_external_hourly_activity.rb
@@ -2,9 +2,9 @@
 
 class RenameExParteActivityToExternalHourlyActivity < ActiveRecord::Migration[8.0]
   def change
-    rename_index :ex_parte_activities, :index_ex_parte_activities_on_member_id, to: :index_external_hourly_activities_on_member_id
-    rename_index :ex_parte_activities, :index_ex_parte_activities_on_period, to: :index_external_hourly_activities_on_period
-    rename_index :ex_parte_activities, :index_ex_parte_activities_on_source, to: :index_external_hourly_activities_on_source
+    rename_index :ex_parte_activities, :index_ex_parte_activities_on_member_id, :index_external_hourly_activities_on_member_id
+    rename_index :ex_parte_activities, :index_ex_parte_activities_on_period, :index_external_hourly_activities_on_period
+    rename_index :ex_parte_activities, :index_ex_parte_activities_on_source, :index_external_hourly_activities_on_source
 
     rename_table :ex_parte_activities, :external_hourly_activities
   end
