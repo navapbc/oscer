7cf0108ec39dd99e3ca8f44f9f348ab0cd244be6
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2026-06-23 09:05:55 -0700

    cleanup outcome

--- reporting-app/app/models/api/certifications/outcome.rb
+++ reporting-app/app/models/api/certifications/outcome.rb
@@ -10,22 +10,19 @@ class Api::Certifications::Outcome < ValueObject
     determination = Determination.where(subject: certification).first
     return unless determination
 
-    obj = Api::Certifications::Outcome.new
+    obj = new(timestamp: determination.created_at)
     if determination.not_compliant? && determination.automated?
         obj.status = "indeterminate"
         obj.reason =  ""
         obj.source =  ""
-        obj.timestamp = determination.created_at
     elsif determination.not_compliant?
         obj.status = "not_compliant"
         obj.reason = ""
         obj.source = ""
-        obj.timestamp = determination.created_at
     else
         obj.status = determination.outcome
         obj.reason = determination.reasons.first
         obj.source = determination.automated? ? "api" : "member"
-        obj.timestamp = determination.created_at
     end
     obj
   end
