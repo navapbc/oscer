4d60d13164f544ebcb549c0b5226787b5a51ee9c
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2026-07-29 13:56:26 -0700

    Update database

---
+++ reporting-app/db/migrate/20260729133636_add_certification_attributes.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddCertificationAttributes < ActiveRecord::Migration[8.0]
+  def change
+    add_column :certifications, :eis_client_id, :string
+    add_column :certifications, :application_date, :date
+    add_column :certifications, :household_data, :jsonb
+  end
+end
